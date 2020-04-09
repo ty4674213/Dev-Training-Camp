49a890b1f29290a5c1106d480d7b0e412b5f0475
Lee <derek.lee@accenture.com>
2020-04-09 22:32:04 +0700

    Initial commit

---
+++ src/main/java/com/example/Dev/Training/Camp/DevTrainingCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dev.Training.Camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTrainingCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldController.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dev/Training/Camp/DevTrainingCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dev.Training.Camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTrainingCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
